6fc81fc49393b79c43c995b5d6600ce225ddd0ec
saulo-albuquerque <saulosoaresfisica@gmail.com>
2025-03-11 14:21:45 +0100

    Update __init__.py
    including the loading of mlgw_NN from the file NN_model

--- mlgw/__init__.py
+++ mlgw/__init__.py
@@ -21,3 +21,4 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from .GW_generator import GW_generator, list_models
 from .GW_generator import mode_generator_base
 from .GW_generator import mode_generator_NN
+from .NN_model import mlgw_NN
