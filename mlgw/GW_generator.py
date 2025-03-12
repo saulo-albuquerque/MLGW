49cc08333eb662fc1ef6078482b31db6bad33a81
saulo-albuquerque <saulosoaresfisica@gmail.com>
2025-03-12 17:53:28 +0100

    Update GW_generator.py
    I added get_red_grads_2 to see if it works

--- mlgw/GW_generator.py
+++ mlgw/GW_generator.py
@@ -2476,6 +2476,66 @@ class mode_generator_MoE(mode_generator_base):
 
 		return grad_amp, grad_ph
 
+	def get_red_grads_2(self, theta):
+		"""
+		Returns the grads of the PCA reduced coefficients w.r.t. the input variables, as estimated by the final trained neural network models.
+
+		Input:
+			theta: :class:`~numpy:numpy.ndarray`
+				shape (N,3) - source parameters to make prediction at
+
+		Output:
+			red_amp,red_ph: :class:`~numpy:numpy.ndarray`
+				shape (N,K,3) - PCA reduced amplitude and phase
+		"""
+		comps_to_list = lambda comps_str: [int(c) for c in comps_str]
+		#new way
+		amp_grad = np.zeros((theta.shape[0], self.amp_PCA.get_dimensions()[1],theta.shape[1]))
+		ph_grad = np.zeros((theta.shape[0], self.ph_PCA.get_dimensions()[1],theta.shape[1]))
+		
+		for comps, model in self.amp_models.items():
+			#amp_pred[:,comps_to_list(comps)] = model(augment_features(theta, model.features)).numpy()
+			input_ = tf.constant(augment_features(theta, model.features).astype(np.float32))
+			with tf.GradientTape() as tape:
+				tape.watch(input_)
+				predict_amp=model(input_)
+				jacobian_amp=tape.jacobian(predict_amp,input_)  
+				for i in range(theta.shape[0]):
+								amp_grad[i]=jacobian_amp[i,:,i] 
+								print(f"{i}",amp_grad[i])   
+				print("jacobian_amp",amp_grad)
+				
+		for comps, model in self.ph_models.items():
+			#ph_pred[:,comps_to_list(comps)] = model(augment_features(theta, model.features)).numpy()
+			input_ = tf.constant(augment_features(theta, model.features).astype(np.float32))
+			with tf.GradientTape() as tape:
+				tape.watch(input_)
+				predict_ph=model(input_)
+				jacobian_ph=tape.jacobian(predict_ph,input_)
+				for i in range(theta.shape[0]):
+								jac=jacobian_ph[i,:,i,:theta.shape[1]]
+								print(f"jac_ph_{comps}",jac)      
+								ph_grad[i,comps_to_list(comps),:]=(jacobian_ph[i,:,i,:theta.shape[1]])
+								print(f"{i}",ph_grad[i])
+				print("jacobian_ph",ph_grad)
+       
+		for comps, model in self.ph_residual_models.items():
+			#ph_pred[:,comps_to_list(comps)] += model(augment_features(theta, model.features)).numpy()*self.ph_res_coefficients[comps]
+			input_ = tf.constant(augment_features(theta, model.features).astype(np.float32))
+			with tf.GradientTape() as tape:
+				tape.watch(input_)
+				predict_res_ph=model(input_)
+				jacobian_res_ph=tape.jacobian(predict_res_ph,input_)
+				for i in range(theta.shape[0]):
+								jac=jacobian_res_ph[i,:,i,:theta.shape[1]]
+								print(f"jac_ph_{comps}",jac)      
+								ph_grad[i,comps_to_list(comps),:]+=(jacobian_res_ph[i,:,i,:theta.shape[1]])
+								print(f"{i}",ph_grad[i])
+				print("jacobian_res_ph",ph_grad)
+				
+		return amp_grad, ph_grad	
+
+
 
 #################
 
